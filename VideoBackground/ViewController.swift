5c7e35ac5df7b81f19c6c66c17e9acf6f3f77586
Alberto Jauregui <bastertwo@gmail.com>
2016-02-17 22:04:28 -0700

    Initial Commit

---
+++ VideoBackground/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoBackground
+//
+//  Created by Alberto Jauregui on 17/02/16.
+//  Copyright © 2016 Alberto Jauregui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
