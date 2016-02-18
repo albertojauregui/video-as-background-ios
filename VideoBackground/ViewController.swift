e627ad00cf28e8de557f1d9cb570f62065326423
Alberto Jauregui <bastertwo@gmail.com>
2016-02-17 22:46:27 -0700

    Added some parallax effect and useful comments

--- VideoBackground/ViewController.swift
+++ VideoBackground/ViewController.swift
@@ -19,18 +19,37 @@ class ViewController: UIViewController {
         let bundle = NSBundle.mainBundle()
         let path = bundle.pathForResource("backgroundVideo", ofType: "mp4")
         player = AVPlayer(URL: NSURL(fileURLWithPath: path!))
+        // Set the actionAtItemEnd is important for the notification part
         player!.actionAtItemEnd = AVPlayerActionAtItemEnd.None
         let playerLayer = AVPlayerLayer(player: player)
         
         playerLayer.frame = self.view.frame
         playerLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
         
-        self.view.layer.addSublayer(playerLayer)
+        // Make sure that the video is added to the back of every layer in the view
+        self.view.layer.insertSublayer(playerLayer, atIndex: 0)
         
+        // Listen for notification when the video reach to the end
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "playerItemDidReachEnd", name: AVPlayerItemDidPlayToEndTimeNotification, object: player!.currentItem)
         
         player!.seekToTime(kCMTimeZero)
         player!.play()
+        
+        // we create a vertical UIMotionEffect
+        let verticalMotionEffect = UIInterpolatingMotionEffect(keyPath: "center.y", type: .TiltAlongVerticalAxis)
+        verticalMotionEffect.minimumRelativeValue = -10
+        verticalMotionEffect.maximumRelativeValue = 10
+        
+        // Another horizontal one
+        let horizontalMotionEffect = UIInterpolatingMotionEffect(keyPath: "center.x", type: .TiltAlongHorizontalAxis)
+        horizontalMotionEffect.minimumRelativeValue = -10
+        horizontalMotionEffect.maximumRelativeValue = 10
+        
+        // Now we create a group of both effects, and assign it to our root view
+        let group = UIMotionEffectGroup()
+        group.motionEffects = [horizontalMotionEffect, verticalMotionEffect]
+        self.view.addMotionEffect(group)
+
     }
     
     func playerItemDidReachEnd() {
